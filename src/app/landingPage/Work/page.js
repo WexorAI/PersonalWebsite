1a41a65e74d3f425c0a31e9252d6a5fcf50fb9a4
abdulrashid <abdulrashidkalodi@gmail.com>
2023-08-16 13:47:21 +0530

    image changed

--- src/app/landingPage/Work/page.js
+++ src/app/landingPage/Work/page.js
@@ -81,7 +81,7 @@ function Work() {
               <div className="group-hover:bg-black/70 w-full absolute z-40 transition-all duration-300 "></div>
               {/* img */}
               <Image
-              src="https://raw.githubusercontent.com/WexorAI/PersonalWebsite/main/public/work.jpgrk.jpg"
+              src="https://raw.githubusercontent.com/WexorAI/PersonalWebsite/main/public/work.jpg"
               alt="Login Image"
               width={800}
               height={750}
