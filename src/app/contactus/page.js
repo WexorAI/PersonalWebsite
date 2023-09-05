c065d2c64adb6e69bc9465b713064edea891a2ed
abdulrashid <abdulrashidkalodi@gmail.com>
2023-09-05 16:01:43 +0530

    contactus configuaration done

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./ContactUs.css";
 import {
   FaArrowLeft,
@@ -7,14 +8,53 @@ import {
   FaPhone,
 } from "react-icons/fa";
 import Link from "next/link";
-// import Link from "next/link";
+import axios from "axios"; // Import Axios
+
 
 function ContactUs() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Send data to your Node.js server
+    axios
+      .post("https://chatbot.wexorai.com/clientmessagef3", {
+        client_id: "wexorai",
+        name: formData.name,
+        email: formData.email,
+        subject: "nothing",
+        message: formData.message,
+      })
+      .then(function (response) {
+        if (response.data.status) {
+          // Email sent successfully, you can show a success message here.
+          alert("Email sent successfully!");
+        } else {
+          // Handle errors here if the email failed to send.
+          alert("Error sending the email. Please try again later.");
+        }
+      })
+      .catch(function (error) {
+        // Handle network or other errors here.
+        console.error("Error:", error);
+        alert("Error sending the email. Please try again later.");
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <div className="soon">
-      <div class="containerr">
-        <div class="wrapperr">
-          <div class="contentt">
+      <div className="containerr">
+        <div className="wrapperr">
+          <div className="contentt">
             <div className="aboutus">
               <div className="containers">
                 <div className="contents">
@@ -68,28 +108,46 @@ function ContactUs() {
                       may have. I'm here to assist you and provide the help you
                       need.
                     </p>
-                    <form action="#">
+                    <form onSubmit={handleFormSubmit}>
                       <div className="input-box">
-                        <input type="text" placeholder="Enter your name" />
+                        <input
+                          type="text"
+                          placeholder="Enter your name"
+                          name="name"
+                          value={formData.name}
+                          onChange={handleInputChange}
+                          required
+                        />
                       </div>
                       <div className="input-box">
-                        <input type="text" placeholder="Enter your email" />
+                        <input
+                          type="email"
+                          placeholder="Enter your email"
+                          name="email"
+                          value={formData.email}
+                          onChange={handleInputChange}
+                          required
+                        />
                       </div>
                       <div className="input-box message-box">
-                        <input type="text" placeholder="Enter your message" />
+                        <input
+                          type="text"
+                          placeholder="Enter your message"
+                          name="message"
+                          value={formData.message}
+                          onChange={handleInputChange}
+                          required
+                        />
                       </div>
                       <div>
-                        {/* <div className="button mr-5 ">
-                  <input type="button" value="Send Now" />
-                </div> */}
                         <div>
                           <Link legacyBehavior href="/">
-                            <a class="button2">🡨 BACK to home</a>
+                            <button className="button2">🡨 BACK to home</button>
                           </Link>
 
-                          <a href="something" class="button2">
-                            sent
-                          </a>
+                          <button type="submit" className="button2">
+                            Send
+                          </button>
                         </div>
                       </div>
                     </form>
@@ -105,3 +163,50 @@ function ContactUs() {
 }
 
 export default ContactUs;
+
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// const ContactUsPage = () => {
+//   const [email, setEmail] = useState('');
+//   const [message, setMessage] = useState('');
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const response = await axios.post('http://127.0.0.1:5000/clientmessagef3', {
+//         email: email,
+//         message: message,
+//       });
+
+//       console.log(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Contact Us</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>Email:</label>
+//         <input
+//           type="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <br />
+//         <label>Message:</label>
+//         <textarea
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//         ></textarea>
+//         <br />
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default ContactUsPage;
